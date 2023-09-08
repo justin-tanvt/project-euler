1e9e19bd225787a26ff1309c1f41c7b3462adc55
justin-tanvt <justintan28@gmail.com>
2023-09-09 03:28:45 +0800

    simplify boolean return in problem 1

--- src/main/java/net/projecteuler/P001.java
+++ src/main/java/net/projecteuler/P001.java
@@ -3,23 +3,19 @@ package net.projecteuler;
 public class P001 {
 
     public static boolean isMultipleOf(int numberToCheck, int factorToCheck) {
-        if (numberToCheck % factorToCheck == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return numberToCheck % factorToCheck == 0;
     }
 
-    public static void main(String[] args) {
-
-        int sum = 0;
-
-        for (int i = 0; i < 1000; i++) {
-            if (isMultipleOf(i,3) || isMultipleOf(i, 5)) {
-                sum += i;
-            }
-        }
-
-        System.out.println(sum);
-    }
+//    public static void main(String[] args) {
+//
+//        int sum = 0;
+//
+//        for (int i = 0; i < 1000; i++) {
+//            if (isMultipleOf(i,3) || isMultipleOf(i, 5)) {
+//                sum += i;
+//            }
+//        }
+//
+//        System.out.println(sum);
+//    }
 }
