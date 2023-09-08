a5030de4712582221469aa11c0271bc1625dc506
ararajs <aravindr002@e.ntu.edu.sg>
2023-09-09 03:02:02 +0800

    add test for problem 17

---
+++ src/test/java/net/projecteuler/P017Test.java
@@ -0,0 +1,21 @@
+package net.projecteuler;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P017Test {
+
+    @Test
+    void checkAllNumbersTo1000() {
+        int result = 0;
+        for (int i = 1; i <= 1000; i++) {
+            result += P017.numberToLetters(i);
+        }
+
+        int expected = 21124;
+        assertEquals(expected, result);
+    }
+}
