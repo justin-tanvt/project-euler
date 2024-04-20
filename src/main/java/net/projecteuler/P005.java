aaf426dbd8f3c34a44726e667d6f08dcf93109ee
justin-tanvt <justintan28@gmail.com>
2024-04-20 12:41:51 +0800

    [AUTO]: checked in from macbook-air

--- src/main/java/net/projecteuler/P005.java
+++ src/main/java/net/projecteuler/P005.java
@@ -2,13 +2,13 @@ package net.projecteuler;
 
 public class P005 {
 
-    public static boolean isDivisibleBy1To20(long n) {
-        int[] factorsToDivideBy = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        for (int factor : factorsToDivideBy) {
-            if (n % factor != 0) {
+    public static boolean isDivisibleByAllNumbersInRangeInclusive(long number, int firstFactor, int lastFactor) {
+        for (int factor = firstFactor; factor <= lastFactor; factor++) {
+            if (number % factor != 0) {
                 return false;
             }
         }
+
         return true;
     }
 
@@ -17,13 +17,16 @@ public class P005 {
         2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
         What is the smallest positive number that is evenly divisible by all the numbers from 1 to 20?
          */
-        long numberToFind = 1;
+        int FACTOR_MIN = 1;
+        int FACTOR_MAX = 20;
+
+        long currentNumber = 1;
         while (true) {
-            if (isDivisibleBy1To20(numberToFind)) {
-                System.out.println(numberToFind);
+            if (isDivisibleByAllNumbersInRangeInclusive(currentNumber, FACTOR_MIN, FACTOR_MAX)) {
+                System.out.println(currentNumber);
                 break;
             } else {
-                numberToFind++;
+                currentNumber++;
             }
         }
     }
