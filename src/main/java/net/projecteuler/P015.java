9d316fbb9d968a14542b1acf636727e3f3ce2ceb
justin-tanvt <justintan28@gmail.com>
2024-04-20 15:06:59 +0800

    implement method for P015 and add rationale

--- src/main/java/net/projecteuler/P015.java
+++ src/main/java/net/projecteuler/P015.java
@@ -1,36 +1,41 @@
 package net.projecteuler;
 
-import java.util.HashSet;
-import java.util.List;
+import java.math.BigInteger;
 
 public class P015 {
-    public static final int GRID_WIDTH = 2;
-    public static final int GRID_HEIGHT = 2;
+    public static final long GRID_SIZE = 20;
 
-    public static HashSet<String> allPaths = new HashSet<>();
-
-    public static int currentX;
-    public static int currentY;
-
-    enum Direction {
-        RIGHT,
-        DOWN
+    public static void main(String[] args) {
+        System.out.println(getNumberOfPossibleRoutes(GRID_SIZE));
     }
 
-    public static int[] posAfterMove(Direction direction) {
-        int[] pos = new int[]{currentX, currentY};
-        switch (direction) {
-            case RIGHT:
-                pos[0]++;
-                break;
-            case DOWN:
-                pos[1]++;
-                break;
+    // given a grid of size X, any route to the bottom right corner has X no. of rightward and downward moves each
+    // the route can be remodeled as a sequence of 2X slots, to be filled by X rightward and X downward moves
+    // a route is made by choosing X out of the 2X slots to fill with a given direction
+    // (after which, the remaining slots have to be in the other direction)
+    // this is effectively a combination of X objects from a set of 2X objects
+    // therefore, the number of routes is the number of unique ways X slots can be chosen from a total of 2X slots
+    public static long getNumberOfPossibleRoutes(long gridSize) {
+        long totalMoves = gridSize * 2;
+        long movesInEachDirection = gridSize;
+        return getNumberOfCombinations(totalMoves, movesInEachDirection);
+    }
+
+    public static long getNumberOfCombinations(long totalNumberOfObjects, long numberOfObjectsPerCombination) {
+        return factorial(totalNumberOfObjects).divide(
+                        factorial(numberOfObjectsPerCombination).multiply(
+                                factorial(totalNumberOfObjects - numberOfObjectsPerCombination)
+                        )
+                ).longValue();
+    }
+
+    public static BigInteger factorial(long n) {
+        BigInteger result = BigInteger.valueOf(1);
+
+        for (long factor = 2; factor <= n; factor++) {
+            result = result.multiply(BigInteger.valueOf(factor));
         }
-        return pos;
-    }
 
-    public static boolean isLegal(int posX, int posY, Direction direction) {
-        return false;
+        return result;
     }
 }
