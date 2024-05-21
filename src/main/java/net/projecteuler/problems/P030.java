99df7badcdb21456b5226a5cfb7e364b1941dff9
justin-tanvt <justintan28@gmail.com>
2024-05-22 01:36:08 +0800

    solve P030

--- src/main/java/net/projecteuler/problems/P030.java
+++ src/main/java/net/projecteuler/problems/P030.java
@@ -12,27 +12,57 @@ public class P030 {
 
     private static final int EXPONENT = 5;
 
+    // 1-digit numbers not included as their digits cannot be summed
+    private static final int NUMBER_MINIMUM_INCLUSIVE = 10;
+
     public static void main(String[] args) {
-        System.out.printf("final solution = %s\n", solve());
+        long startTime = System.nanoTime();
+        System.out.printf("\nfinal solution = %s\n", solve());
+        long endTime = System.nanoTime();
+        System.out.printf("\nsolution execution time = %s ms\n", (endTime - startTime) / 1000000);
     }
 
     private static String solve() {
         int maxDigits = maxDigitsForValidSolution();
         Set<Integer> solutions = new HashSet<>();
 
-        int number = 10;        // start at minimum 2 digit number
+        int number = NUMBER_MINIMUM_INCLUSIVE;
+        while (Integer.toString(number).length() <= maxDigits) {
+            if (satisfiesExpression(number)) {
+                solutions.add(number);
+            }
+            number++;
+        }
 
-        return "";
+        return Integer.toString(solutions.stream().mapToInt(i -> i).sum());
+    }
+
+    private static boolean satisfiesExpression(int number) {
+        int sum = 0;
+
+        String numStr = Integer.toString(number);
+        for (int i = 0; i < numStr.length(); i++) {
+            int digit = Character.digit(numStr.charAt(i), 10);
+            sum += (int) Math.pow(digit, EXPONENT);
+        }
+
+        if (sum == number) {
+            System.out.printf("%s satisfies the expression\n", number);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private static int maxDigitsForValidSolution() {
         // for a given number of digits, there is a range of values for the LHS and RHS of the expression
-        // i.e. for 2 digits, the LHS has range [10, 99] while the RHS is [1^EXPONENT*2, 9^EXPONENT*2]
+        // for 2 digits, the LHS has range [10, 99] while the RHS is [1*2, 9^EXPONENT*2]
+        // for 3 digits, the LHS' range is [100, 999] while the RHS is [1*3, 9^EXPONENT*2], and so on...
 
         // a solution can only be found when both ranges intersect (i.e. the LHS COULD equal to the RHS)
         // as digits are increased, it is observed that LHS' range increases faster than RHS' range
         // therefore, there is a maximum number of digits beyond which the LHS can no longer equal to the RHS
-        // this is the number of digits before which the LHS' minimum value exceeds the RHS' maximum value
+        // this is the number of digits before the LHS' minimum value exceeds the RHS' maximum value
 
         int numOfDigits = 1;
         int LHSmin;
